d2d8700212d2e3c489aee81aa1285ee1b1a6f958
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-10-18 21:59:57 -0300

    Create produto and produto builder

--- Builder/src/main/java/org/example/Main.java
+++ Builder/src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Produto produto = new ProdutoBuilder()
+                .descricao("Televisao")
+                .marca("LG")
+                .createProduto();
+
+        System.out.println(produto);
     }
 }
