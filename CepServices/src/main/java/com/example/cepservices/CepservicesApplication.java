d9ae6515a59a3f6acd97ab7a6226f2ea67b656ab
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-10-25 19:37:13 -0300

    Create Spring project

---
+++ CepServices/src/main/java/com/example/cepservices/CepservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cepservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepservicesApplication.class, args);
+	}
+
+}
---
+++ CepServices/src/test/java/com/example/cepservices/CepservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cepservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
