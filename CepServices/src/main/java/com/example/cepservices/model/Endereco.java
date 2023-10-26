8a9a1080508e5ebf5c0d7395f1ce270ad06c0379
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-10-25 21:59:14 -0300

    Adapt service to use another apis

--- CepServices/src/main/java/com/example/cepservices/model/Endereco.java
+++ CepServices/src/main/java/com/example/cepservices/model/Endereco.java
@@ -1,14 +1,25 @@
 package com.example.cepservices.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 @Data
 public class Endereco {
+    @JsonAlias("code")
     private String cep;
+
+    @JsonAlias({"address", "endereco"})
     private String logradouro;
-    private String complemento;
+
+    @JsonAlias("district")
     private String bairro;
+
+    @JsonAlias({"city", "cidade"})
     private String localidade;
+
+    @JsonAlias({"state", "estado"})
     private String uf;
+
+    private String complemento;
     private String ibge;
 }
--- CepServices/src/main/java/com/example/cepservices/service/CepService.java
+++ CepServices/src/main/java/com/example/cepservices/service/CepService.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "https://viacep.com.br/ws", name="viacep")
+//@FeignClient(url = "https://viacep.com.br/ws", name="viacep")
+@FeignClient(url = "https://ws.apicep.com", name="apicep")
 public interface CepService {
-    @GetMapping("/{cep}/json")
+//    @GetMapping("/{cep}/json")
+    @GetMapping("/cep/{cep}.json")
     Endereco getEndereco(@PathVariable String cep);
 }
